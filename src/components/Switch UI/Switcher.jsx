c30ae6f4a3a63ca6f8870d0be6248826a070ec57
Olim Yuldoshev <oyuldoshev39@gmail.com>
2024-01-24 10:40:45 +0500

    Added some properties of dark mode for controllng

--- src/components/Switch UI/Switcher.jsx
+++ src/components/Switch UI/Switcher.jsx
@@ -13,6 +13,41 @@ export default function Switcher() {
     setTheme(colorTheme);
     setDarkSide(checked);
     };
+
+
+  const defaultProperties = {
+    dark: {
+      circle: {
+        r: 9,
+      },
+      mask: {
+        cx: "50%",
+        cy: "23%",
+      },
+      svg: {
+        transform: "rotate(40deg)",
+      },
+      lines: {
+        opacity: 0,
+      },
+    },
+    light: {
+      circle: {
+        r: 5,
+      },
+      mask: {
+        cx: "100%",
+        cy: "0%",
+      },
+      svg: {
+        transform: "rotate(90deg)",
+      },
+      lines: {
+        opacity: 1,
+      },
+    },
+    springConfig: { mass: 4, tension: 250, friction: 35 },
+  };
     
   return (
     <>
@@ -23,6 +58,7 @@ export default function Switcher() {
           size={27}
           moonColor="yellow"
           sunColor="orange"
+          animationProperties={defaultProperties}
         />
         
       </div>
